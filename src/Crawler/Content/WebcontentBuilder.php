2e2eed2f5f174011ecefc14869bde4f1739b2785
Michele Capicchioni <capimichi@gmail.com>
2016-11-15 00:01:22 +0100

    Added webbuilder

---
+++ src/Crawler/Content/WebcontentBuilder.php
@@ -0,0 +1,11 @@
+<?php
+namespace Crawler\Content;
+
+/**
+ * https://dzone.com/articles/factories-builders-and-fluent-
+ * Class WebContentBuilder
+ * @package Crawler\Content
+ */
+abstract class WebContentBuilder{
+
+}
