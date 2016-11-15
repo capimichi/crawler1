59b9200ad52b47fc7e39bce1c42844c2e781aa1c
Michele Capicchioni <capimichi@gmail.com>
2016-11-15 18:43:29 +0100

    Created fieldbuilder content and fieldtypes enum

--- src/Crawler/Single/Fields/FieldBuilder.php
+++ src/Crawler/Single/Fields/FieldBuilder.php
@@ -1,4 +1,98 @@
 <?php
-abstract class FieldBuilder{
+namespace Crawler\Single\Fields;
 
+use Crawler\Selector;
+
+/**
+ * Class FieldBuilder
+ * @package Crawler\Single\Fields
+ */
+class FieldBuilder
+{
+
+    /**
+     * @var Field
+     */
+    protected $field;
+
+
+    /**
+     * FieldBuilder constructor.
+     * @param int $type
+     */
+    public function __construct($type)
+    {
+        switch ($type){
+            case FieldTypes::STRING:
+                $this->field = new FieldString();
+                break;
+            case FieldTypes::URL:
+                $this->field = new FieldUrl();
+                break;
+            case FieldTypes::IMAGE:
+                $this->field = new FieldImage();
+                break;
+        }
+        $this->field->setSelectors(array());
+        $this->field->setMultiple(false);
+    }
+
+
+    /**
+     * @param Selector $selector
+     * @return FieldBuilder $this
+     */
+    public function addSelector($selector)
+    {
+        $selectors = $this->field->getSelectors();
+        $selectors[] = $selector;
+        $this->field->setSelectors($selectors);
+        return $this;
+    }
+
+
+    /**
+     * @param string $name
+     * @return FieldBuilder $this
+     */
+    public function setName($name)
+    {
+        $this->field->setName($name);
+        return $this;
+    }
+
+    /**
+     * @param bool $multiple
+     * @return FieldBuilder $this
+     */
+    public function setMultiple($multiple)
+    {
+        $this->field->setMultiple($multiple);
+        return $this;
+    }
+
+    /**
+     * @return FieldBuilder $this
+     * @throws \Exception
+     */
+    public function validate()
+    {
+        if(count($this->field->getSelectors()) < 1){
+            throw new \Exception("Selectors not defined");
+        }
+
+        if(empty($this->field->getName())){
+            throw new \Exception("Name not defined");
+        }
+        return $this;
+    }
+
+    /**
+     * @return Field
+     */
+    public function build()
+    {
+        $this->validate();
+        return $this->field;
+    }
 }
---
+++ src/Crawler/Single/Fields/FieldTypes.php
@@ -0,0 +1,12 @@
+<?php
+namespace Crawler\Single\Fields;
+
+/**
+ * Class FieldTypes
+ * @package Crawler\Single\Fields
+ */
+class FieldTypes{
+    const STRING = 0;
+    const URL = 1;
+    const IMAGE = 2;
+}
